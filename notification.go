64568429b892b78e823c94a0f7dbf8a9d4d4b095
edwinlab <edwinuad@gmail.com>
2019-11-27 11:04:06 +0700

    add notificaiton option

--- notification.go
+++ notification.go
@@ -19,6 +19,17 @@ type NotificationMessage struct {
 	InstanceId string `json:"instance_id,omitempty"`
 }
 
+type AppSettingGroup struct {
+	Group string                 `json:"group,omitempty"`
+	Title string                 `json:"title,omitempty"`
+	Items []*AppSettingGroupItem `json:"items,omitempty"`
+}
+
+type AppSettingGroupItem struct {
+	Key   string   `json:"key,omitempty"`
+	Value string   `json:"value,omitempty"`
+}
+
 func (c *Client) CreateNotification(ctx context.Context, token string, req RequestCreateNotificationMessage) (*Response, error) {
 	url := fmt.Sprintf("%s/notifications/internal/messages", c.url)
 	return c.walk(http.MethodPost, url, token, req)
@@ -26,5 +37,10 @@ func (c *Client) CreateNotification(ctx context.Context, token string, req Reque
 
 func (c *Client) NotificationList(ctx context.Context, token string, userID string) (*Response, error) {
 	url := fmt.Sprintf("%s/notifications/internal/options?user_id=%s", c.url, userID)
+	return c.walk(http.MethodGet, url, token, nil)
+}
+
+func (c *Client) NotificationOption(ctx context.Context, token string, userID string) (*Response, error) {
+	url := fmt.Sprintf("%s/settings/internal/settings?user_id=%s", c.url, userID)
 	return c.walk(http.MethodGet, url, token, nil)
 }
